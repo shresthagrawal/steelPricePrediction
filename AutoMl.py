1d10c001387eb56cfd44b8b621a75ab84119b559
shresth <shresthagrawal.31@gmail.com>
2018-10-10 08:08:46 +0530

    imported the csv file

--- AutoMl.py
+++ AutoMl.py
@@ -1,9 +1,17 @@
 
 import numpy
+import csv
 
+data =[]
+#dataset = numpy.loadtxt("raw.csv", delimiter=",")
+with open('raw.csv') as csvfile:
+     spamreader = csv.DictReader(csvfile)
+     for row in spamreader:
+         if row['Prices']!= '-':
+             data.append(row)
+         
 
-dataset = numpy.loadtxt("raw.csv", delimiter=",")
+print(data[0]['Prices'])
 # split into input (X) and output (Y) variables
-X = dataset[:,0:6]
-Y = dataset[:,6]
-print(dataset)
+#X = dataset[:,0:6]
+#Y = dataset[:,6]
